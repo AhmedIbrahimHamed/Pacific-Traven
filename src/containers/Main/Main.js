23d4061cc8b0718152d58c59d0aeeae2733a946d
AhmedIbrahimHamed <aih1175@gmail.com>
2020-03-19 14:44:43 +0200

    Initial Commit

---
+++ src/containers/Main/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Main
