bde0e65261cb0aaf032703931aed1d88d0bac2b3
AhmedIbrahimHamed <aih1175@gmail.com>
2020-03-22 19:26:16 +0200

    Updates

---
+++ precache-manifest.8b874dd3bdd5abf97320b53aa0fa5eb0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27421deba46809534f6cf0c432c35b6",
+    "url": "/Pacific-Traven/index.html"
+  },
+  {
+    "revision": "9f582a9beff25f14e8ce",
+    "url": "/Pacific-Traven/static/css/2.7c96d096.chunk.css"
+  },
+  {
+    "revision": "d6d1ca349a0d1a02d6b0",
+    "url": "/Pacific-Traven/static/css/main.f700ccc6.chunk.css"
+  },
+  {
+    "revision": "9f582a9beff25f14e8ce",
+    "url": "/Pacific-Traven/static/js/2.b07e012a.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Pacific-Traven/static/js/2.b07e012a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d1ca349a0d1a02d6b0",
+    "url": "/Pacific-Traven/static/js/main.53303084.chunk.js"
+  },
+  {
+    "revision": "a65591f2d6c757461c38",
+    "url": "/Pacific-Traven/static/js/runtime-main.6a3bde1f.js"
+  },
+  {
+    "revision": "f539f7f8df835cbe0192df2d8ca57220",
+    "url": "/Pacific-Traven/static/media/10748178545_3e1e4056b9_k.f539f7f8.jpg"
+  },
+  {
+    "revision": "0cabb2f94dfa7feee0c96ae4d8970ffa",
+    "url": "/Pacific-Traven/static/media/10748270326_e94eeb2435_k.0cabb2f9.jpg"
+  },
+  {
+    "revision": "b63e96cd678e3abaf3ff106c1e1f2afc",
+    "url": "/Pacific-Traven/static/media/12324630754_04f0a12dfc_mod.b63e96cd.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Pacific-Traven/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Pacific-Traven/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Pacific-Traven/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Pacific-Traven/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Pacific-Traven/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25651f22512a300d849dcf787f721e12",
+    "url": "/Pacific-Traven/static/media/header-Image.25651f22.jpg"
+  },
+  {
+    "revision": "4da6b8db9660b8e1b249c46e6d7879a6",
+    "url": "/Pacific-Traven/static/media/pacific-logo.4da6b8db.png"
+  }
+]);
